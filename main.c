b1e55353de567316802b75aebd223879479b22df
Liam Chatland <101197780+LiamChats@users.noreply.github.com>
2022-03-13 22:48:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**
  * Title: Lab Assesment #1 - Dice Game Implementation 
  * Course: CST8234 C Language
- * @author: Kadija Allagouma
+ * @author: Kadija Allagouma & Liam Chatland
  * Due date: 2022-03-13
  * Submission date: 03/13/22
  */
@@ -24,7 +24,7 @@
  Function in parameter: void
  Funciton out parameter: EXIT_SUCCESS
  Version: 1
- Author: Kadija Allagouma
+ Author: Kadija Allagouma and Liam Chatland
 ***************************************************************************************************************/
 int main(){
     here:
@@ -33,28 +33,24 @@ int main(){
         int result;
         do{
             if(balance<=0){
-                char userInput;
-                printf("You have no more funds. Start over? [y/n]?\n");
-                scanf("%s",&userInput);
-                if(userInput == 'y'){
-                    goto here;
-                }else if (userInput == 'n'){
-                    printf("Exiting...");
-                    exit(EXIT_SUCCESS);
-                }else{
-                    printf("Invalid input.. try again");
+                while(1){
+                    char userInput;
+                    printf("You have no more funds. Start over? [y/n]?\n");
+                    scanf("%s",&userInput);
+                    if(userInput == 'y'){
+                        goto here;
+                    }else if (userInput == 'n'){
+                        printf("Thank you for playing! GoodBye...");
+                        exit(EXIT_SUCCESS);
+                    }else{
+                        printf("Invalid input.. try again\n");
+                    }
                 }
             }
             printf("You Have %d Dollars!\n",balance);
             int result = playGame(&balance);
         }while(1);
-   /*if(result == WON){
-       printf("YOU WON!\n");
-   } else if (result == LOSE){
-       printf("YOU LOST!\n");
-   } else {
-        printf("Exiting\n");
-   }*/
+   
     
     return EXIT_SUCCESS;
 }

eaa3f4a3e80f427c7857c5950fe482f0d674a433
Liam Chatland <101197780+LiamChats@users.noreply.github.com>
2022-03-13 21:46:40 -0400

    Update rollDice.h

--- rollDice.h
+++ rollDice.h
@@ -1,13 +1,3 @@
-
-/********************************************************************************
-Function:               rollDice()
-Purpose:                Generate a random number in between 1-6
-Function In Parameters: Void
-Return Value:           Returns the Number variable which holds the generated number
-********************************************************************************/
-
 int rollDice(void){
-    int die;
-    die = rand()%7;
-    return("%d\n", die);
+    return(rand()%7);
 }
